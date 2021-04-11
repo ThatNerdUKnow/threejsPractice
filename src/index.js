ff5885e56a8ed82ec28e08a2fb13f593e73e9478
Brandon Pina <brandonpi265@gmail.com>
2021-04-11 05:51:46 -0500

    set build to production mode

--- src/index.js
+++ src/index.js
@@ -79,3 +79,20 @@ function animate() {
   renderer.render(scene, camera);
 }
 animate();
+
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  console.log("resize");
+});
